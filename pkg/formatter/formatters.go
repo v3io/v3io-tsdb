fd3a6410becce3f26e19606e104d6eb9115dc487
Yaron Haviv <yaronh@iguaz.io>
2018-07-05 20:14:56 +0300

    fix iterator Err handling in Formatters

--- pkg/formatter/formatters.go
+++ pkg/formatter/formatters.go
@@ -14,25 +14,26 @@ type textFormatter struct {
 func (f textFormatter) Write(out io.Writer, set querier.SeriesSet) error {
 
 	for set.Next() {
-		if set.Err() != nil {
-			return set.Err()
-		}
-
 		series := set.At()
 		name, lbls := labelsToStr(series.Labels())
 		fmt.Fprintf(out, "Name: %s  Labels: %s\n", name, lbls)
 		iter := series.Iterator()
 		for iter.Next() {
-
-			if iter.Err() != nil {
-				return iter.Err()
-			}
-
 			t, v := iter.At()
 			fmt.Fprintf(out,"  %s  v=%.2f\n", f.timeString(t), v)
 		}
+
+		if iter.Err() != nil {
+			return iter.Err()
+		}
+
 		fmt.Fprintln(out,"")
 	}
+
+	if set.Err() != nil {
+		return set.Err()
+	}
+
 
 	return nil
 }
@@ -46,9 +47,6 @@ func (f csvFormatter) Write(out io.Writer, set querier.SeriesSet) error {
 
 	writer := csv.NewWriter(out)
 	for set.Next() {
-		if set.Err() != nil {
-			return set.Err()
-		}
 
 		series := set.At()
 		name, labelStr := labelsToStr(series.Labels())
@@ -56,13 +54,17 @@ func (f csvFormatter) Write(out io.Writer, set querier.SeriesSet) error {
 		iter := series.Iterator()
 		for iter.Next() {
 
-			if iter.Err() != nil {
-				return iter.Err()
-			}
-
 			t, v := iter.At()
 			writer.Write([]string{name, labelStr, fmt.Sprintf("%.6f", v), f.timeString(t)})
 		}
+
+		if iter.Err() != nil {
+			return iter.Err()
+		}
+	}
+
+	if set.Err() != nil {
+		return set.Err()
 	}
 
 	writer.Flush()
@@ -85,10 +87,6 @@ func (f simpleJsonFormatter) Write(out io.Writer, set querier.SeriesSet) error {
 	output := "["
 
 	for set.Next() {
-		if set.Err() != nil {
-			return set.Err()
-		}
-
 		series := set.At()
 		name, labelStr := labelsToStr(series.Labels())
 		datapoints := ""
@@ -96,10 +94,6 @@ func (f simpleJsonFormatter) Write(out io.Writer, set querier.SeriesSet) error {
 		iter := series.Iterator()
 		firstItem := true
 		for iter.Next() {
-
-			if iter.Err() != nil {
-				return iter.Err()
-			}
 
 			t, v := iter.At()
 			if !firstItem {
@@ -109,11 +103,19 @@ func (f simpleJsonFormatter) Write(out io.Writer, set querier.SeriesSet) error {
 			firstItem = false
 		}
 
+		if iter.Err() != nil {
+			return iter.Err()
+		}
+
 		if !firstSeries {
 			output = output + ","
 		}
 		output = output + fmt.Sprintf(metricTemplate, name, labelStr, datapoints)
 		firstSeries = false
+	}
+
+	if set.Err() != nil {
+		return set.Err()
 	}
 
 	_, err := out.Write([]byte(output + "\n]"))
