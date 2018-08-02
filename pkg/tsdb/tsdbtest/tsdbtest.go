cfadd46ed9a4d78b4962a59a51d18b0a1a54abb0
Tal Neiman <33829179+talIguaz@users.noreply.github.com>
2018-08-02 18:56:15 +0300

    Add tests (#28)
    * add aggregators test
    
    * add ingest+query+create tests
    
    * rename tests description, add negative aggregator test.
    
    * add integration target to make file + pr comments
    
    * add bug id to ignored tests
    
    * go format
    
    * add a test
    
    * format
    
    * create nuclio build tag, create test util
    
    * format
    
    * run every table test case as a sub test
    
    * more pr comments + move tests util to tsdbtest
    
    * move test models to tsdbtest
    
    * support ignoring tests
    
    * create unique DB name on test setup
    
    * format
    
    * add delete TSDB test
    
    * minor style fix
    
    * fix nuclio test
    
    * format
    
    * style changes
    
    * improve tests + use testify
    
    * add build tags to go get
    
    * fix tags on go get

---
+++ pkg/tsdb/tsdbtest/tsdbtest.go
@@ -0,0 +1,59 @@
+package tsdbtest
+
+import (
+	"fmt"
+	"github.com/v3io/v3io-tsdb/pkg/config"
+	. "github.com/v3io/v3io-tsdb/pkg/tsdb"
+	"github.com/v3io/v3io-tsdb/pkg/utils"
+	"testing"
+	"time"
+)
+
+type DataPoint struct {
+	Time  int64
+	Value float64
+}
+
+type Sample struct {
+	Lset  utils.Labels
+	Time  string
+	Value float64
+}
+
+func DeleteTSDB(t *testing.T, v3ioConfig *config.V3ioConfig) {
+	adapter, err := NewV3ioAdapter(v3ioConfig, nil, nil)
+	if err != nil {
+		t.Fatalf("Failed to create adapter. reason: %s", err)
+	}
+
+	if err := adapter.DeleteDB(true, true); err != nil {
+		t.Fatalf("Failed to delete DB on teardown. reason: %s", err)
+	}
+}
+
+func SetUp(t *testing.T, v3ioConfig *config.V3ioConfig) func() {
+	dbConfig := config.DBPartConfig{
+		DaysPerObj:     1,
+		HrInChunk:      1,
+		DefaultRollups: "sum",
+		RollupMin:      10,
+	}
+	v3ioConfig.Path = fmt.Sprintf("%s-%d", v3ioConfig.Path, time.Now().Nanosecond())
+	if err := CreateTSDB(v3ioConfig, &dbConfig); err != nil {
+		t.Fatalf("Failed to create TSDB. reason: %s", err)
+	}
+
+	return func() {
+		DeleteTSDB(t, v3ioConfig)
+	}
+}
+
+func SetUpWithDBConfig(t *testing.T, v3ioConfig *config.V3ioConfig, dbConfig config.DBPartConfig) func() {
+	if err := CreateTSDB(v3ioConfig, &dbConfig); err != nil {
+		t.Fatalf("Failed to create TSDB. reason: %s", err)
+	}
+
+	return func() {
+		DeleteTSDB(t, v3ioConfig)
+	}
+}
--- test/benchmark/BenchmarkIngestWithNuclio_test.go
+++ test/benchmark/BenchmarkIngestWithNuclio_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/v3io/v3io-go-http"
 	"github.com/v3io/v3io-tsdb/pkg/config"
 	"github.com/v3io/v3io-tsdb/pkg/tsdb"
+	"github.com/v3io/v3io-tsdb/pkg/tsdb/tsdbtest"
 	"github.com/v3io/v3io-tsdb/pkg/utils"
 	"github.com/v3io/v3io-tsdb/test/benchmark/common"
 	"io/ioutil"
@@ -151,7 +152,7 @@ func initContext(context *nuclio.Context) error {
 
 func handler(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
 
-	sample := common.Sample{}
+	sample := tsdbtest.Sample{}
 	err := json.Unmarshal(event.GetBody(), &sample)
 	if err != nil {
 		return nil, err
