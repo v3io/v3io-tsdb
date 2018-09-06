9238f74c2d9b2515a5810d656ec1f235ae88daab
Gal Topper <galt@iguaz.io>
2018-09-06 09:48:26 +0300

    Added unit tests.

---
+++ pkg/utils/timeutils_test.go
@@ -0,0 +1,54 @@
+// +build unit
+
+/*
+Copyright 2018 Iguazio Systems Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License") with
+an addition restriction as set forth herein. You may not use this
+file except in compliance with the License. You may obtain a copy of
+the License at http://www.apache.org/licenses/LICENSE-2.0.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing
+permissions and limitations under the License.
+
+In addition, you may not use the software for any purposes that are
+illegal under applicable law, and the grant of the foregoing license
+under the Apache 2.0 license is conditioned upon your compliance with
+such restriction.
+*/
+
+package utils
+
+import (
+	"github.com/stretchr/testify/suite"
+	"testing"
+	"time"
+)
+
+type testTimeSuite struct {
+	suite.Suite
+}
+
+func (suite *testTimeSuite) TestStr2durationOneHour() {
+	res, err := Str2duration("1h")
+	suite.Require().Nil(err)
+	suite.Require().Equal(int64(time.Hour/time.Millisecond), res)
+}
+
+func (suite *testTimeSuite) TestStr2durationZero() {
+	res, err := Str2duration("0")
+	suite.Require().Nil(err)
+	suite.Require().Equal(int64(0), res)
+}
+
+func (suite *testTimeSuite) TestStr2durationNegative() {
+	_, err := Str2duration("-1")
+	suite.Require().Error(err)
+}
+
+func TestTimeSuite(t *testing.T) {
+	suite.Run(t, new(testTimeSuite))
+}
