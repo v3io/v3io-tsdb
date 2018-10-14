714b35c2948ffa656c307551b906efed78bd953f
Sharon Lifshitz <sharonl@iguaz.io>
2018-10-14 16:05:30 +0300

    [DOC] CLI: Edit log-level help text & comments (#172)
    * [DOC] CLI: minor log-level & web-APIs service comments editing
    
    * [DOC] CLI -v|--log-level help: change example in light of default-level changes
    
    * [DOC] CLI: edit the -v|--log-level help text
    
    * [DOC] CLI help: minor -v|--log-level edit
    
    * CLI: Use the config.DefaultLogLevel const to set the default log level

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -51,6 +51,7 @@ const (
 	DefaultAggregationGranularity = "1h"
 	DefaultLayerRetentionTime     = "1y"
 	DefaultSampleRetentionTime    = 0
+	DefaultLogLevel               = "info"
 	DefaultVerboseLevel           = "debug"
 )
 
@@ -77,7 +78,7 @@ type V3ioConfig struct {
 	// Disabled = true disables the V3IO TSDB configuration in Prometheus and
 	// enables the internal Prometheus TSDB instead
 	Disabled bool `json:"disabled,omitempty"`
-	// Logging level (for verbose output) - "debug" | "info" | "warn" | "error"
+	// Log level - "debug" | "info" | "warn" | "error"
 	LogLevel string `json:"logLevel,omitempty"`
 	// Number of parallel V3IO worker routines
 	Workers int `json:"workers"`
--- pkg/tsdbctl/tsdbctl.go
+++ pkg/tsdbctl/tsdbctl.go
@@ -61,7 +61,7 @@ func NewRootCommandeer() *RootCommandeer {
 	defaultV3ioServer := os.Getenv("V3IO_SERVICE_URL")
 
 	cmd.PersistentFlags().StringVarP(&commandeer.logLevel, "log-level", "v", "",
-		"Verbose output. You can provide one of the following logging\nlevels as an argument for this flag by using the assignment\noperator ('='): \"debug\" | \"info\" | \"warn\" | \"error\".\nFor example: -v=info. The default log level when using this\nflag without an argument is \""+config.DefaultVerboseLevel+"\".")
+		"Verbose output. Add \"=<level>\" to set the log level -\ndebug | info | warn | error. For example: -v=warn.\n(default - \""+config.DefaultVerboseLevel+"\" when using the flag; \""+config.DefaultLogLevel+"\" otherwise)")
 	cmd.PersistentFlags().Lookup("log-level").NoOptDefVal = config.DefaultVerboseLevel
 	cmd.PersistentFlags().StringVarP(&commandeer.dbPath, "table-path", "t", "",
 		"[Required] Path to the TSDB table within the configured\ndata container. Examples: \"mytsdb\"; \"/my_tsdbs/tsdbd1\".")
@@ -192,7 +192,7 @@ func (rc *RootCommandeer) populateConfig(cfg *config.V3ioConfig) error {
 	if rc.logLevel != "" {
 		cfg.LogLevel = rc.logLevel
 	} else {
-		cfg.LogLevel = "info"
+		cfg.LogLevel = config.DefaultLogLevel
 	}
 
 	rc.v3iocfg = cfg
