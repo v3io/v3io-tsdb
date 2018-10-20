0216f60be04b65f070088a9a2a7ee0376563b0c3
Yaron Haviv <yaronh@iguaz.io>
2018-10-21 02:28:14 +0300

    start new parallel querier WIP

---
+++ pkg/pquerier/collector.go
@@ -0,0 +1,50 @@
+package pquerier
+
+/* main query flow logic
+
+fire GetItems to all partitions and tables
+
+iterate over results from first to last partition
+	hash lookup (over labels only w/o name) to find dataFrame
+		if not found create new dataFrame
+	based on hash dispatch work to one of the parallel collectors
+	collectors convert raw/array data to series and aggregate or group
+
+once collectors are done (wg.done) return SeriesSet (prom compatible) or FrameSet (iguazio column interface)
+	final aggregators (Avg, Stddav/var, ..) are formed from raw aggr in flight via iterators
+	- Series: have a single name and optional aggregator per time series, values limited to Float64
+	- Frames: have index/time column(s) and multiple named/typed value columns (one per metric name * function)
+
+** optionally can return SeriesSet (from dataFrames) to Prom immediately after we completed GetItems iterators
+   and block (wg.done) the first time Prom tries to access the SeriesIter data (can lower latency)
+
+   if result set needs to be ordered we can also sort the dataFrames based on Labels data (e.g. order-by username)
+   in parallel to having all the time series data processed by the collectors
+
+*/
+
+/*  collector logic:
+
+- get qryResults from chan
+
+- if raw query
+	if first partition
+		create series
+	  else
+		append chunks to existing series
+
+- if vector query (results are bucketed over time or grouped by)
+	if first partition
+		create & init array per function (and per name) based on query metadata/results
+	  else
+		?
+
+	init child raw-chunk or attribute iterators
+	iterate over data and fill bucketed arrays
+		if missing time or data use interpolation
+
+- if got fin message and processed last item
+	use sync waitgroup to signal the main that the go routines are done
+	will allow main flow to continue and serve the results, no locks are required
+
+*/
---
+++ pkg/pquerier/types.go
@@ -0,0 +1,76 @@
+package pquerier
+
+import (
+	"github.com/v3io/v3io-tsdb/pkg/utils"
+)
+
+// data and metadata passed to the query processor workers via a channel
+type qryResults struct {
+	frame     *dataFrame
+	part      *partGetter
+	name      string
+	fields    *map[string]interface{}
+	partIndex int
+	encoding  int8
+	columns   *map[string][]columnMeta
+}
+
+type dataFrame struct {
+	lset        utils.Labels
+	hash        uint64
+	columnNames []string
+	rawColumns  []Series
+
+	// bucketColumns
+	// indexColumn
+
+}
+
+// columns metadata for applying various functions and grouping on data
+type functionType int16
+
+type columnMeta struct {
+	alias          string
+	function       functionType
+	functionParams []interface{}
+	interpolator   int8
+}
+
+// SeriesSet contains a set of series.
+type SeriesSet interface {
+	Next() bool
+	At() Series
+	Err() error
+}
+
+// Null-series set
+type nullSeriesSet struct {
+	err error
+}
+
+func (s nullSeriesSet) Next() bool { return false }
+func (s nullSeriesSet) At() Series { return nil }
+func (s nullSeriesSet) Err() error { return s.err }
+
+// Series represents a single time series.
+type Series interface {
+	// Labels returns the complete set of labels identifying the series.
+	Labels() utils.Labels
+	// Iterator returns a new iterator of the data of the series.
+	Iterator() SeriesIterator
+	// Unique key for sorting
+	GetKey() uint64
+}
+
+// SeriesIterator iterates over the data of a time series.
+type SeriesIterator interface {
+	// Seek advances the iterator forward to the given timestamp.
+	// If there's no value exactly at t, it advances to the first value after t.
+	Seek(t int64) bool
+	// At returns the current timestamp/value pair.
+	At() (t int64, v float64)
+	// Next advances the iterator by one.
+	Next() bool
+	// Err returns the current error.
+	Err() error
+}
