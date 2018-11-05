57a338bde8728f3c1ca966a0835a3e4759e00d46
Gal Topper <galt@iguaz.io>
2018-11-05 12:34:12 +0200

    Use timestamps, use NoError.

--- pkg/tsdb/tsdb_integration_test.go
+++ pkg/tsdb/tsdb_integration_test.go
@@ -39,40 +39,43 @@ type testTsdbSuite struct {
 func (suite *testTsdbSuite) TestAppend() {
 
 	v3ioConfig, err := tsdbtest.LoadV3ioConfig()
-	suite.Require().Nil(err)
+	suite.Require().NoError(err)
 
 	defer tsdbtest.SetUp(suite.T(), v3ioConfig)()
 
 	adapter, err := tsdb.NewV3ioAdapter(v3ioConfig, nil, nil)
-	suite.Require().Nil(err)
+	suite.Require().NoError(err)
 
 	appender, err := adapter.Appender()
-	suite.Require().Nil(err)
+	suite.Require().NoError(err)
 
 	querier, err := adapter.Querier(nil, 0, math.MaxInt64)
-	suite.Require().Nil(err)
+	suite.Require().NoError(err)
+
+	t1 := suite.parseTime("2018-11-01T00:00:00Z")
+	t2 := suite.parseTime("2018-11-03T00:00:00Z")
 
 	_, err = appender.Add(
 		utils.Labels{utils.Label{Name: "__name__", Value: "AAPL"}, utils.Label{Name: "market", Value: "usa"}},
-		1541030400000,
+		t1,
 		-91)
-	suite.Require().Nil(err)
+	suite.Require().NoError(err)
 	_, err = appender.Add(
 		utils.Labels{utils.Label{Name: "__name__", Value: "AAL"}, utils.Label{Name: "market", Value: "usa"}},
-		1541030400000,
+		t1,
 		-87)
-	suite.Require().Nil(err)
+	suite.Require().NoError(err)
 	_, err = appender.Add(
 		utils.Labels{utils.Label{Name: "__name__", Value: "AAP"}, utils.Label{Name: "market", Value: "usa"}},
-		1541203200000,
+		t2,
 		-50)
-	suite.Require().Nil(err)
+	suite.Require().NoError(err)
 
 	_, err = appender.WaitForCompletion(0)
-	suite.Require().Nil(err)
+	suite.Require().NoError(err)
 
 	set, err := querier.Select("", "min", int64(time.Hour/time.Millisecond), "1==1")
-	suite.Require().Nil(err)
+	suite.Require().NoError(err)
 
 	// TODO: Replace map[tv]struct{} with []tv once TSDB-37 is fixed. This open issue causes duplicate results.
 	var result = make(map[string]map[tv]struct{})
@@ -89,12 +92,18 @@ func (suite *testTsdbSuite) TestAppend() {
 	}
 
 	expected := map[string]map[tv]struct{}{
-		`{__name__="AAPL", market="usa", Aggregate="min"}`: {tv{t: 1541030400000, v: -91}: struct{}{}},
-		`{__name__="AAL", market="usa", Aggregate="min"}`:  {tv{t: 1541030400000, v: -87}: struct{}{}},
-		`{__name__="AAP", market="usa", Aggregate="min"}`:  {tv{t: 1541203200000, v: -50}: struct{}{}},
+		`{__name__="AAPL", market="usa", Aggregate="min"}`: {tv{t: t1, v: -91}: struct{}{}},
+		`{__name__="AAL", market="usa", Aggregate="min"}`:  {tv{t: t1, v: -87}: struct{}{}},
+		`{__name__="AAP", market="usa", Aggregate="min"}`:  {tv{t: t2, v: -50}: struct{}{}},
 	}
 
 	suite.Require().Equal(expected, result)
+}
+
+func (suite *testTsdbSuite) parseTime(timestamp string) int64 {
+	t, err := time.Parse(time.RFC3339, timestamp)
+	suite.Require().NoError(err)
+	return t.Unix() * 1000
 }
 
 type tv struct {
