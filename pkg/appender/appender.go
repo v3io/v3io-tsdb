db1da7bd6c38bd39e98429dcc7aadd6c7ace3979
Gal Topper <galt@iguaz.io>
2018-12-23 12:24:41 +0200

    Fix nil pointer
    resulting from merge between pre-aggregations and variant stuff.

--- pkg/appender/appender.go
+++ pkg/appender/appender.go
@@ -233,11 +233,11 @@ func (mc *MetricsCache) Add(lset utils.LabelsIfc, t int64, v interface{}) (uint6
 				}
 			}
 		}
+		metric = &MetricState{Lset: lset, key: key, name: name, hash: hash}
 		// if the (first) value is not float, use variant encoding, TODO: test w schema
 		if _, ok := v.(float64); !ok {
 			metric.isVariant = true
 		}
-		metric = &MetricState{Lset: lset, key: key, name: name, hash: hash}
 		metric.store = NewChunkStore(mc.logger, false)
 		mc.addMetric(hash, name, metric)
 	} else {
