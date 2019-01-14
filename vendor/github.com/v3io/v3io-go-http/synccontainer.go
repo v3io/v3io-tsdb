9d3951e1890f39e37e96b145c2fc2d3bc074282a
Golan <gshatz@gmail.com>
2019-01-15 00:54:05 +0200

    1. release processed chunks 2. validate getItems response to avoid infinite loop

--- vendor/github.com/v3io/v3io-go-http/synccontainer.go
+++ vendor/github.com/v3io/v3io-go-http/synccontainer.go
@@ -236,6 +236,15 @@ func (sc *SyncContainer) GetItems(input *GetItemsInput) (*Response, error) {
 		return nil, err
 	}
 
+	//validate range scan loop
+	if getItemsResponse.LastItemIncluded != "TRUE" && (getItemsResponse.NextMarker == "" || getItemsResponse.NextMarker == input.Marker) {
+		errMsg := fmt.Sprintf("Invalid getItems response: lastItemIncluded='false' && nextMarker='%s', " +
+			"startMarker='%s', probably due to object size bigger than 2M. Query is: %+v", getItemsResponse.NextMarker, input.Marker, input)
+		sc.logger.Error(errMsg)
+		return nil, errors.New(errMsg)
+	}
+
+
 	getItemsOutput := GetItemsOutput{
 		NextMarker: getItemsResponse.NextMarker,
 		Last:       getItemsResponse.LastItemIncluded == "TRUE",
