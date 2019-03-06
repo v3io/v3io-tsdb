ec6bac975fefc36f190b5ed04badd8f406271082
Tal Neiman <33829179+talIguaz@users.noreply.github.com>
2019-03-06 23:25:25 +0200

    fix wrong calculation of queryCtx.isRawQuery() + change the global query step if needed (#316)

--- pkg/pquerier/query_integration_test.go
+++ pkg/pquerier/query_integration_test.go
@@ -2976,6 +2976,64 @@ func (suite *testQuerySuite) TestQueryDataFrameMultipleMetricsWithMultipleLabelS
 	assert.Equal(suite.T(), 2, seriesCount, "series count didn't match expected")
 }
 
+func (suite *testQuerySuite) TestAggregatesWithDisabledClientAggregation() {
+	suite.v3ioConfig.DisableClientAggr = true
+	defer func() { suite.v3ioConfig.DisableClientAggr = false }()
+
+	adapter, err := tsdb.NewV3ioAdapter(suite.v3ioConfig, nil, nil)
+	if err != nil {
+		suite.T().Fatalf("failed to create v3io adapter. reason: %s", err)
+	}
+	labels1 := utils.LabelsFromStringList("os", "linux")
+	numberOfEvents := 10
+	eventsInterval := 60 * 1000
+
+	ingestedData := []tsdbtest.DataPoint{{suite.basicQueryTime - tsdbtest.DaysInMillis, 10},
+		{int64(suite.basicQueryTime + tsdbtest.MinuteInMillis), 20},
+		{suite.basicQueryTime + 2*tsdbtest.MinuteInMillis, 30},
+		{suite.basicQueryTime + 3*tsdbtest.MinuteInMillis, 40}}
+	testParams := tsdbtest.NewTestParams(suite.T(),
+		tsdbtest.TestOption{
+			Key: tsdbtest.OptTimeSeries,
+			Value: tsdbtest.TimeSeries{tsdbtest.Metric{
+				Name:   "cpu",
+				Labels: labels1,
+				Data:   ingestedData},
+			}})
+	tsdbtest.InsertData(suite.T(), testParams)
+
+	expected := map[string][]tsdbtest.DataPoint{"avg": {{Time: suite.basicQueryTime - tsdbtest.DaysInMillis, Value: 10},
+		{Time: suite.basicQueryTime, Value: 30}}}
+
+	querierV2, err := adapter.QuerierV2()
+	if err != nil {
+		suite.T().Fatalf("Failed to create querier v2, err: %v", err)
+	}
+
+	params := &pquerier.SelectParams{Name: "cpu", Functions: "avg", From: suite.basicQueryTime - tsdbtest.DaysInMillis, To: suite.basicQueryTime + int64(numberOfEvents*eventsInterval)}
+	set, err := querierV2.Select(params)
+	if err != nil {
+		suite.T().Fatalf("Failed to exeute query, err: %v", err)
+	}
+
+	var seriesCount int
+	for set.Next() {
+		seriesCount++
+		iter := set.At().Iterator()
+
+		data, err := tsdbtest.IteratorToSlice(iter)
+		agg := set.At().Labels().Get(aggregate.AggregateLabel)
+		if err != nil {
+			suite.T().Fatal(err)
+		}
+		currentExpected, ok := expected[agg]
+		suite.Require().Equal(true, ok, "got unexpected aggregate result")
+		assert.Equal(suite.T(), currentExpected, data, "queried data does not match expected")
+	}
+
+	assert.Equal(suite.T(), 1, seriesCount, "series count didn't match expected")
+}
+
 func (suite *testQuerySuite) TestDifferentLabelSetsInDifferentPartitions() {
 	adapter, err := tsdb.NewV3ioAdapter(suite.v3ioConfig, nil, nil)
 	if err != nil {
