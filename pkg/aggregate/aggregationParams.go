d9e258c00a2d196ce7f233aacbd356d013d54a10
Tal Neiman <33829179+talIguaz@users.noreply.github.com>
2019-03-18 15:15:53 +0200

    IG-11142 Support aggregation window (#323)
    * support aggregation window
    
    * add comment
    
    * go fmt
    
    * fix server side aggregation bug
    
    * add aggregation-window argument to the CLI
    
    * fix aggregation with window bug if the start query time is exactly a start of a partition
    
    * windowed aggregation across partitions fix + address pr comments
    
    * fix iterator bug
    
    * make windowing calculations clearer
    
    * address pr comments
    
    * pr comments

--- pkg/aggregate/aggregationParams.go
+++ pkg/aggregate/aggregationParams.go
@@ -6,15 +6,16 @@ import (
 )
 
 type AggregationParams struct {
-	colName        string   // column name ("v" in timeseries)
-	aggrMask       AggrType // the sum of aggregates (or between all aggregates)
-	rollupTime     int64    // time per bucket (cell in the array)
-	Interval       int64    // requested (query) aggregation step
-	buckets        int      // number of buckets in the array
-	overlapWindows []int    // a list of overlapping windows (* interval), e.g. last 1hr, 6hr, 12hr, 24hr
+	colName           string   // column name ("v" in timeseries)
+	aggrMask          AggrType // the sum of aggregates (or between all aggregates)
+	rollupTime        int64    // time per bucket (cell in the array)
+	Interval          int64    // requested (query) aggregation step
+	buckets           int      // number of buckets in the array
+	overlapWindows    []int    // a list of overlapping windows (* interval), e.g. last 1hr, 6hr, 12hr, 24hr
+	aggregationWindow int64    // a time window on which to calculate the aggregation per Interval
 }
 
-func NewAggregationParams(functions, col string, buckets int, interval, rollupTime int64, windows []int) (*AggregationParams, error) {
+func NewAggregationParams(functions, col string, buckets int, interval, aggregationWindow, rollupTime int64, windows []int) (*AggregationParams, error) {
 
 	aggregatesList := strings.Split(functions, ",")
 	aggrMask, _, err := AggregatesFromStringListWithCount(aggregatesList)
@@ -23,12 +24,13 @@ func NewAggregationParams(functions, col string, buckets int, interval, rollupTi
 	}
 
 	newAggregateSeries := AggregationParams{
-		aggrMask:       aggrMask,
-		colName:        col,
-		buckets:        buckets,
-		rollupTime:     rollupTime,
-		Interval:       interval,
-		overlapWindows: windows,
+		aggrMask:          aggrMask,
+		colName:           col,
+		buckets:           buckets,
+		rollupTime:        rollupTime,
+		aggregationWindow: aggregationWindow,
+		Interval:          interval,
+		overlapWindows:    windows,
 	}
 
 	return &newAggregateSeries, nil
@@ -42,7 +44,9 @@ func (as *AggregationParams) CanAggregate(partitionAggr AggrType) bool {
 	// if interval and rollup are not even divisors we need higher resolution (3x) to smooth the graph
 	// when we add linear/spline graph projection we can reduce back to 1x
 	return ((aggrMask & partitionAggr) == aggrMask) &&
-		as.Interval >= as.rollupTime && (as.Interval%as.rollupTime == 0 || as.Interval/as.rollupTime > 3)
+		as.Interval >= as.rollupTime &&
+		(as.Interval%as.rollupTime == 0 || as.Interval/as.rollupTime > 3) &&
+		(as.aggregationWindow == 0 || as.aggregationWindow >= as.rollupTime)
 }
 
 func (as *AggregationParams) GetAggrMask() AggrType {
@@ -51,6 +55,14 @@ func (as *AggregationParams) GetAggrMask() AggrType {
 
 func (as *AggregationParams) GetRollupTime() int64 {
 	return as.rollupTime
+}
+
+func (as *AggregationParams) GetAggregationWindow() int64 {
+	return as.aggregationWindow
+}
+
+func (as *AggregationParams) HasAggregationWindow() bool {
+	return as.aggregationWindow > 0
 }
 
 func (as *AggregationParams) toAttrName(aggr AggrType) string {
--- pkg/pquerier/chunkIterator.go
+++ pkg/pquerier/chunkIterator.go
@@ -12,7 +12,7 @@ import (
 
 // Chunk-list series iterator
 type RawChunkIterator struct {
-	mint, maxt int64
+	mint, maxt, aggregationWindow int64
 
 	chunks   []chunkenc.Chunk
 	encoding chunkenc.Encoding
@@ -33,8 +33,16 @@ func newRawChunkIterator(queryResult *qryResults, log logger.Logger) utils.Serie
 		maxt = int64(maxTime.(int))
 	}
 
+	var aggregationWindow int64
+	if queryResult.query.aggregationParams != nil {
+		aggregationWindow = queryResult.query.aggregationParams.GetAggregationWindow()
+	}
 	newIterator := RawChunkIterator{
-		mint: queryResult.query.mint, maxt: maxt, log: log.GetChild("rawChunkIterator"), encoding: queryResult.encoding}
+		mint:              queryResult.query.mint,
+		maxt:              maxt,
+		aggregationWindow: aggregationWindow,
+		log:               log.GetChild("rawChunkIterator"),
+		encoding:          queryResult.encoding}
 
 	newIterator.AddChunks(queryResult)
 
@@ -56,8 +64,8 @@ func (it *RawChunkIterator) Seek(t int64) bool {
 	}
 
 	// Seek to the first valid value after t
-	if t < it.mint {
-		t = it.mint
+	if t < it.mint-it.aggregationWindow {
+		t = it.mint - it.aggregationWindow
 	}
 
 	// Check the first element
@@ -115,7 +123,7 @@ func (it *RawChunkIterator) Next() bool {
 	it.updatePrevPoint()
 	if it.iter.Next() {
 		t, _ := it.iter.At()
-		if t < it.mint {
+		if t < it.mint-it.aggregationWindow {
 			if !it.Seek(it.mint) {
 				return false
 			}
