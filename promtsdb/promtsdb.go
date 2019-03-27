9a7752d1be623c4e42fe1cb6aaf08c03b55ef3b3
Gal Topper <galt@iguaz.io>
2019-03-27 11:47:53 +0200

    Update Prometheus version from 2.5.0 to 2.8.0.

--- promtsdb/promtsdb.go
+++ promtsdb/promtsdb.go
@@ -69,7 +69,7 @@ type V3ioPromQuerier struct {
 }
 
 // Select returns a set of series that matches the given label matchers.
-func (promQuery *V3ioPromQuerier) Select(params *storage.SelectParams, oms ...*labels.Matcher) (storage.SeriesSet, error) {
+func (promQuery *V3ioPromQuerier) Select(params *storage.SelectParams, oms ...*labels.Matcher) (storage.SeriesSet, error, storage.Warnings) {
 	name, filter, functions := match2filter(oms, promQuery.logger)
 	noAggr := false
 
@@ -77,10 +77,10 @@ func (promQuery *V3ioPromQuerier) Select(params *storage.SelectParams, oms ...*l
 	if params == nil {
 		labelSets, err := promQuery.v3ioQuerier.GetLabelSets(name, filter)
 		if err != nil {
-			return nil, err
+			return nil, err, nil
 		}
 
-		return &V3ioPromSeriesSet{newMetadataSeriesSet(labelSets)}, nil
+		return &V3ioPromSeriesSet{newMetadataSeriesSet(labelSets)}, nil, nil
 	}
 
 	promQuery.logger.Debug("SelectParams: %+v", params)
@@ -106,7 +106,7 @@ func (promQuery *V3ioPromQuerier) Select(params *storage.SelectParams, oms ...*l
 		To:        promQuery.maxt}
 
 	set, err := promQuery.v3ioQuerier.SelectProm(selectParams, noAggr)
-	return &V3ioPromSeriesSet{s: set}, err
+	return &V3ioPromSeriesSet{s: set}, err, nil
 }
 
 // LabelValues returns all potential values for a label name.
