4f504280aab8382002cfe4b790d8f76fa35f4796
Dina Nimrodi <dinan@iguaz.io>
2020-03-22 12:39:02 +0200

    remove numWorkers check

--- pkg/pquerier/querier.go
+++ pkg/pquerier/querier.go
@@ -150,11 +150,6 @@ func (q *V3ioQuerier) baseSelectQry(params *SelectParams, showAggregateLabel boo
 		return nil, errors.Wrap(err, "Failed to read/update the TSDB schema.")
 	}
 
-	// TODO: should be checked in config
-	if q.cfg.QryWorkers == 0 {
-		return nil, errors.New("query workers num must be > 0")
-	}
-
 	// If the config is set to use only client configuration override the query parameter.
 	if q.cfg.UsePreciseAggregations {
 		params.UseOnlyClientAggr = true
